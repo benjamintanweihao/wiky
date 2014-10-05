5567f57bf2ee0778cd67d497fb8fb7f00f285011
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2014-10-06 01:07:54 +0800

    Link start button to parse Wikipedia.

--- priv/static/js/controllers/wiky-parser.coffee
+++ priv/static/js/controllers/wiky-parser.coffee
@@ -2,19 +2,18 @@ angular.module('wiky')
   .controller 'WikyParserCtrl', [
     '$scope', '$http',
     ($scope ,  $http) ->
-      $scope.progressText       = "Not started."
-      $scope.progressPercentage = 30
-
+      $scope.progressText       = "not started"
+      $scope.progressPercentage = 0
       socket = new Phoenix.Socket("/ws")
-
-      socket.join("wiky-parser-channel", "start-parser", {}, (channel) ->
-        channel.on("progress", (message) ->
-          $scope.progressPercentage = message.progress_percentage
-          $scope.progressText       = message.progress_status
-          $scope.$apply($scope.progressPercentage)
-          $scope.$apply($scope.progressText)
+       
+      $scope.start = ->
+        socket.join("wiky-parser-channel", "start-parser", {}, (channel) ->
+          channel.on("progress", (message) ->
+            $scope.progressPercentage = message.progress_percentage
+            $scope.progressText       = message.progress_status
+            $scope.$apply($scope.progressPercentage)
+            $scope.$apply($scope.progressText)
+          )
         )
-      )
-
-        
   ]
+
--- priv/static/js/controllers/wiky-parser.js
+++ priv/static/js/controllers/wiky-parser.js
@@ -2,17 +2,19 @@
   angular.module('wiky').controller('WikyParserCtrl', [
     '$scope', '$http', function($scope, $http) {
       var socket;
-      $scope.progressText = "Not started.";
-      $scope.progressPercentage = 30;
+      $scope.progressText = "not started";
+      $scope.progressPercentage = 0;
       socket = new Phoenix.Socket("/ws");
-      return socket.join("wiky-parser-channel", "start-parser", {}, function(channel) {
-        return channel.on("progress", function(message) {
-          $scope.progressPercentage = message.progress_percentage;
-          $scope.progressText = message.progress_status;
-          $scope.$apply($scope.progressPercentage);
-          return $scope.$apply($scope.progressText);
+      return $scope.start = function() {
+        return socket.join("wiky-parser-channel", "start-parser", {}, function(channel) {
+          return channel.on("progress", function(message) {
+            $scope.progressPercentage = message.progress_percentage;
+            $scope.progressText = message.progress_status;
+            $scope.$apply($scope.progressPercentage);
+            return $scope.$apply($scope.progressText);
+          });
         });
-      });
+      };
     }
   ]);
 
