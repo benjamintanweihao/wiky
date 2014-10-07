45dbb0760ecb8f1ab3e74b4db309ba0bde903763
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2014-10-07 15:28:18 +0800

    Clean up words.

--- lib/wiky/markov/worker.ex
+++ lib/wiky/markov/worker.ex
@@ -21,18 +21,27 @@ defmodule Wiky.Markov.Worker do
   end
 
   def build_dictionary([prefix, suffix|rest]) do
-    case Dictionary.get_suffixes(prefix) do
-      nil ->
-        Dictionary.update_suffix(prefix, [suffix])
-      suffixes ->
-        Dictionary.update_suffix(prefix, [suffix|suffixes])
+    if word?(suffix) do
+      case Dictionary.get_suffixes(prefix) do
+        nil ->
+          Dictionary.update_suffix(prefix, [suffix])
+        suffixes ->
+          Dictionary.update_suffix(prefix, [suffix|suffixes])
+      end
+      build_dictionary([suffix|rest])
+    else
+      build_dictionary(rest)
     end
-    build_dictionary([suffix|rest])
   end
 
   def build_dictionary(_source) do
     :ok
   end
+
+  def word?(word) do
+    Regex.match? ~r/^[a-zA-Z]+$/, word
+  end
+
 
 end
 
